cb31cd77404ebe7abb6e3fd3700363a004c61dda
RamseyKeyes <sdalygaming@hotmail.com>
2019-06-22 01:47:46 +1000

    Updated

--- TwitchTweeter/TwitchTweeter.py
+++ TwitchTweeter/TwitchTweeter.py
@@ -5,7 +5,7 @@ class TwitchTweeter(commands.Cog):
     """My custom cog"""
 
     @commands.command()
-    async def mycom(self, ctx):
+    async def setupTweet(self, ctx):
         """This does stuff!"""
         # Your code will go here
         await ctx.send("it WORK!!!! YE HA")
