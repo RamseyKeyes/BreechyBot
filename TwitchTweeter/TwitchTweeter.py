2535802213c1486a4c74bbe3de30d18579812d73
RamseyKeyes <sdalygaming@hotmail.com>
2019-06-22 13:52:06 +1000

    Updated comments

--- TwitchTweeter/TwitchTweeter.py
+++ TwitchTweeter/TwitchTweeter.py
@@ -2,7 +2,7 @@ from redbot.core import commands
 
 
 class TwitchTweeter(commands.Cog):
-    """My custom cog"""
+    """Posts a Tweet when streamers go live"""
 
     @commands.command()
     async def setupTweet(self, ctx):
