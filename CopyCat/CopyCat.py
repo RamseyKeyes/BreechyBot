3a79d83963aadb31b93f2f26d325c723abbe8c16
RamseyKeyes <sdalygaming@hotmail.com>
2019-06-22 16:44:08 +1000

    Updated comments

--- CopyCat/CopyCat.py
+++ CopyCat/CopyCat.py
@@ -2,11 +2,13 @@ from redbot.core import commands
 
 
 class CopyCat(commands.Cog):
-    """" Copys what th user inputs"""
+    """" Copys what the user inputs"""
 
     @commands.command()
     async def mimic(self, ctx, *, words):
         """Monkey Do"""
         await ctx.send(words+" Helo world" )
+
+        await ctx.send("I can do stuff!")
 
 
