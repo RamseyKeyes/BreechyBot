36f192ffe0ddc0de2650f112aa65d64d0c8e1829
RamseyKeyes <sdalygaming@hotmail.com>
2019-06-26 00:06:54 +1000

    Added React Roles to the Repo

--- TwitchTweeter/TwitchTweeter.py
+++ TwitchTweeter/TwitchTweeter.py
@@ -1,7 +1,7 @@
 from redbot.core import commands
 
 
-class TwitchwTweeter(commands.Cog):
+class TwitchTweeter(commands.Cog):
     """Posts a Tweet when streamers go live"""
 
     @commands.command()
